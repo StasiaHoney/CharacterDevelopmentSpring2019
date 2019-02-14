1a8d86e09640e6fd5f981230980853b35f7c92bd
Unknown <kosukeinuzuka@gmail.com>
2019-02-14 12:50:27 -0700

    character controller lecture

---
+++ CharacterDevelopmentSpring2019/Assets/Scripts/MoveCharacter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCharacter : MonoBehaviour
+{
+
+	private CharacterController CC;
+	private Vector3 position;
+	public float MoveSpeed = 5;
+	public float Gravity = -9.81f;
+	void Start ()
+	{
+		CC = GetComponent<CharacterController>();
+	}
+	
+	
+	void Update ()
+	{
+		position.z = Input.GetAxis("Horizontal") * MoveSpeed * Time.deltaTime;
+		if (CC.isGrounded)
+		{
+			
+		}
+		position.y = Gravity * Time.deltaTime; 
+		CC.Move(position);
+	}
+}
