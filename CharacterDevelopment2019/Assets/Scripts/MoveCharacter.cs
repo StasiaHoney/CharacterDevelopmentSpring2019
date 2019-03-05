53900ecddd09fba6ed1be6e5494eb3abcd451d0a
Unknown <kosukeinuzuka@gmail.com>
2019-03-05 13:02:31 -0700

    new character controller and double click script

---
+++ CharacterDevelopment2019/Assets/Scripts/MoveCharacter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MoveCharacter : MonoBehaviour
+{
+    public UnityEvent OnGrounded, OffGrounded;
+    public MoveBase Move;
+    private CharacterController controller;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        if (controller.isGrounded)
+        {
+            
+        }
+    }
+}
+
